dd57d0f5d3fd8cc1bc9b085f14521c00d224a6f9
THETOUCH <super.ilgiznex@gmail.com>
2023-11-07 00:48:47 +0600

    123

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,19 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+//        return x -> x.stream().map(i -> i / divider).collect(Collectors.toList());
+        return x -> {
+            List<Integer> list = new ArrayList<>();
+          for (Integer s : x) {
+            list.add(s/divider);
+          }
+          return list;
+        };
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 }
